d7de0a5c12b6be099aa8694ec8c99bcdfe3d7533
Natalia Skiba <nataliaskiba@Natalias-MacBook-Pro.local>
2021-02-01 00:02:17 -0600

    connection

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const products = [
+	{
+		item: 'spiced rum',
+		brand: 'Captain Morgan',
+		units: '375ml',
+		quantity: 11.99,
+		isPurchased: false
+	},
+	{
+		item: 'tequila',
+		brand: 'Casamigos',
+		units: '750ml',
+		quantity: 38.99,
+		isPurchased: false
+	},
+	{
+		item: 'vodka',
+		brand: 'Titos',
+		units: '1L',
+		quantity: 25.99,
+		isPurchased: false
+	}
+];
+
+export default products;
